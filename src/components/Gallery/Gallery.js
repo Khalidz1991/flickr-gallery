fd66bceaf592301190cee4ca2058b52747ba5666
KhalidManasra <1991.khalid.m@gmail.com>
2018-06-05 22:47:58 +0200

    task1+2+3(a,b)

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,22 +2,64 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Image from '../Image';
 import './Gallery.scss';
+import config from 'config';
+import keys  from '../../config/keys.js';
+
+console.log(keys);
+var FlickerTimer = null;
+var resizeTimer = null;
+var page = 1;
+var isLoading = false;
 
 class Gallery extends React.Component {
   static propTypes = {
-    tag: PropTypes.string
+    tag: PropTypes.string,
   };
 
   constructor(props) {
     super(props);
+    this.deleteHandler = this.deleteHandler.bind(this);
+    this.onResize = this.onResize.bind(this);
     this.state = {
       images: [],
-      galleryWidth: this.getGalleryWidth()
+      galleryWidth: this.getGalleryWidth(),
     };
   }
 
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  onScroll = (w,h,q,e) => {
+    if (
+      (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 2000) && this.state.images.length
+    ) {
+      if(!isLoading){
+        page++;
+        console.log(page);
+        isLoading = true;
+        this.getImages(this.props.tag)
+      }
+    }
+  }
+  onResize = (w,h,q,e) => {
+    var gallary = this;
+    console.log("onResize")
+    if(resizeTimer)
+      clearTimeout(resizeTimer)
+      resizeTimer = setTimeout(function () {
+        gallary.setState({
+          galleryWidth : gallary.getGalleryWidth(),
+        })
+    },1000)
+    
+  }
+ 
+
   getGalleryWidth(){
     try {
+      console.log(document.body.clientWidth)
       return document.body.clientWidth;
     } catch (e) {
       return 1000;
@@ -25,28 +67,57 @@ class Gallery extends React.Component {
   }
 
   getImages(tag) {
-    // TODO: Get images from Flickr
+    console.log(process,process.env,process.env.REACT_APP_FLICKER_API_KEY);
+    if(FlickerTimer)
+      clearTimeout(FlickerTimer);
+      var gallary = this;
+      FlickerTimer = setTimeout(function() {
+      localStorage.setItem('tag',tag)
+      fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='+keys.apiKey+'&tags='+tag+'&page='+page+'&format=json&nojsoncallback=1')
+      .then(function(response){
+        return response.json();
+      }).then(function(j){
+        console.log(j);
+        let picArray = j.photos.photo.map((pic) => {
+          
+          var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+          return pic;
+        })
+        gallary.setState({images: this.state.images.concat(picArray)});
+        isLoading = false;
+      }.bind(gallary))
+    },2000)
+    
   }
 
   componentDidMount() {
+    window.addEventListener('scroll', this.onScroll, false);
+    window.addEventListener('resize', this.onResize, false);
     this.getImages(this.props.tag);
     this.setState({
-      galleryWidth: document.body.clientWidth
+      galleryWidth: document.body.clientWidth,
+      images : this.state.images
     });
   }
 
   componentWillReceiveProps(props) {
+    this.state.images = [];
     this.getImages(props.tag);
   }
-
+  deleteHandler(dto){
+    this.state.images.splice(this.state.images.indexOf(dto),1);
+    this.setState({
+      images  : this.state.images,
+    })
+  }
   render() {
-    return (
-      <div className="gallery-root">
-        {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
-        })}
-      </div>
-    );
+   return (
+     <div className="gallery-root">
+       {this.state.images.map((dto,index) => {
+         return <Image  index={index} key={'image-' + dto.id} dto={dto} onChange={this.deleteHandler} galleryWidth={this.state.galleryWidth}/>;
+       })}
+     </div>
+   );
   }
 }
 
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -6,28 +6,39 @@ import './Image.scss';
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
-    galleryWidth: PropTypes.number
+    galleryWidth: PropTypes.number,
+    onChange:   React.PropTypes.func,
   };
 
   constructor(props) {
+    console.log("constructor")
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onRotate = this.onRotate.bind(this);
+    this.onExpand = this.onExpand.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      rotateDegree : 0,
+      expand : false
     };
   }
 
-  calcImageSize() {
-    const {galleryWidth} = this.props;
+  calcImageSize(newWidth) {
+    const galleryWidth = newWidth || this.props.galleryWidth;
     const targetSize = 200;
-    const imagesPerRow = Math.round(galleryWidth / targetSize);
-    const size = (galleryWidth / imagesPerRow);
+    const imagesPerRow = Math.floor(galleryWidth / targetSize);
+    var extra = (galleryWidth - imagesPerRow*targetSize)/imagesPerRow;
+    const size = 200+extra;
     this.setState({
-      size
+      size : size
     });
   }
-
+  componentWillReceiveProps(nextProps){
+    this.calcImageSize(nextProps.galleryWidth);
+  }
   componentDidMount() {
+    console.log("componentDidMount")
     this.calcImageSize();
   }
 
@@ -35,22 +46,74 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+  onDelete(){
+    this.props.onChange(this.props.dto);
+  }
+  onRotate(){
+    var newDegree = this.state.rotateDegree == 270 ? 0 : this.state.rotateDegree+90 
+    this.setState({
+      rotateDegree : newDegree
+    })
+  }
+  onExpand(){
+
+    this.setState({
+      size : !this.state.expand ?  document.body.clientWidth/2 : 200,
+      expand : !this.state.expand
+    })
+  }
+  
+
   render() {
-    return (
-      <div
-        className="image-root"
-        style={{
-          backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
-        }}
-        >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+    let className = 'image-root';
+    if(this.state.expand){
+       return  (
+          <div  className='modal-overlay'>
+            <div className='modal-body'>
+
+            <div
+          className={className}
+          style={{
+            backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
+            width: this.state.size + 'px',
+            height: this.state.size + 'px',
+            transform : 'rotate('+(this.state.rotateDegree)+'deg)'
+          }}
+          >
+            <div
+              style={{
+                transform: 'rotate('+(this.state.rotateDegree*-1)+'deg)'
+              }}
+            >
+            <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.onRotate} />
+            <FontAwesome className="image-icon"  name="trash-alt" title="delete" onClick={this.onDelete}/>
+            <FontAwesome className="image-icon" name="expand" title="expand" onClick={this.onExpand}/>
+          </div>
         </div>
-      </div>
+
+          </div>
+        </div>)
+    }
+    return (
+        <div 
+          className={className}
+          style={{
+            backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
+            width: this.state.size + 'px',
+            height: this.state.size + 'px',
+            transform : 'rotate('+(this.state.rotateDegree)+'deg)'
+          }}
+          >
+          <div
+          style={{
+            transform: 'rotate('+(this.state.rotateDegree*-1)+'deg)'
+          }}
+            >
+            <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.onRotate} />
+            <FontAwesome className="image-icon"  name="trash-alt" title="delete" onClick={this.onDelete}/>
+            <FontAwesome className="image-icon" name="expand" title="expand" onClick={this.onExpand}/>
+          </div>
+        </div>
     );
   }
 }
